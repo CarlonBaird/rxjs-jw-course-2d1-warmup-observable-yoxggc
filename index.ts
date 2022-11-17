66b95f60ff560f4f70070fc0dc4c7d39f60187a9
CarlonBaird <carlyestrella@live.com>
2022-11-17 15:25:55 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,12 +3,26 @@ import { Observable } from 'rxjs';
 const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
+  setTimeout(() => subscriber.next('Ben'), 2000);
+  setTimeout(() => subscriber.next('Charlie'), 4000);
 });
 
-const observer = {
-  next: (value: any) => console.log(value),
-};
+// const observer = {
+//   next: (value: any) => console.log(value),
+// };
 
-observable$.subscribe(observer);
+// const subscription = observable$.subscribe(observer);
+
+// setTimeout(() => {
+//   console.log('Unsubscribe');
+//   subscription.unsubscribe();
+// }, 3000);
+
+console.log('Subscription 1 starts');
+observable$.subscribe((value) => console.log('Subscription 1:', value));
+
+setTimeout(() => {
+  //after 1000 ms, execute the arrow function
+  console.log('Subscription 2 starts');
+  observable$.subscribe((value) => console.log('Subscription 2:', value));
+}, 1000);
